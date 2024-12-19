e57ddf67487519a64e2d0bc0dc70945f634c1de1
Iñigo <PascualBogajoI@gmail.com>
2024-12-19 10:50:20 +0100

    TJP_SP1_17122024- Metodos SpringBoot

--- api/src/main/java/com/TownPi/api/dto/PuebloDto.java
+++ api/src/main/java/com/TownPi/api/dto/PuebloDto.java
@@ -1,5 +1,77 @@
+
 package com.TownPi.api.dto;
 
-public class PuebloDto {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PuebloDto {
+    private String nombre, descripcion, categorias, localidad, nombreProvincia;
+    private int id, idProvincia;
+    private static int n=0;
+
+
+    
+
+    public PuebloDto(String nombre, String descripcion, String categorias, String localidad, String nombreProvincia,
+            int idProvincia) {
+        n++;
+        id=n;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categorias = categorias;
+        this.localidad = localidad;
+        this.nombreProvincia = nombreProvincia;
+        this.idProvincia = idProvincia;
+    }
+
+    public PuebloDto() {
+
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public String getCategorias() {
+        return categorias;
+    }
+    public String getLocalidad() {
+        return localidad;
+    }
+    public String getNombreProvincia() {
+        return nombreProvincia;
+    }
+    public int getId() {
+        return id;
+    }
+    public int getIdProvincia() {
+        return idProvincia;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public void setCategorias(String categorias) {
+        this.categorias = categorias;
+    }
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+    public void setNombreProvincia(String nombreProvincia) {
+        this.nombreProvincia = nombreProvincia;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setIdProvincia(int idProvincia) {
+        this.idProvincia = idProvincia;
+    }
+
+    
+    
+    
 }
--- api/src/main/java/com/TownPi/api/rest/TownPiApiRestController.java
+++ api/src/main/java/com/TownPi/api/rest/TownPiApiRestController.java
@@ -1,5 +1,91 @@
 package com.TownPi.api.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.TownPi.api.dto.PuebloDto;
+import com.google.gson.Gson;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/api/pueblos")
 public class TownPiApiRestController {
 
+    // Método GET para obtener la lista de pueblos (con filtrado)
+    @GetMapping("/mostrar/{nombre}")
+    public String getPueblos(@PathVariable String nombre){
+        Gson gson = new Gson();
+        PuebloDto puebloAMostrar= new PuebloDto();
+        for (PuebloDto indice : pueblos) {
+            if(indice.getNombre().equals(nombre)){
+                puebloAMostrar= indice;
+            }
+        }
+        return gson.toJson(puebloAMostrar);
+    }
+    @GetMapping("/mostrar")
+        public String getCoches(){
+        Gson gson = new Gson();
+        return gson.toJson(pueblos);  
+    }
+    public List<PuebloDto> pueblos = new ArrayList<>();
+
+
+    // Método POST para añadir un nuevo pueblo
+    @PostMapping("/crear")
+    public String añadirPueblo(@RequestBody PuebloDto pueblo) {
+        Gson gson= new Gson();
+        pueblos.add(pueblo);
+        return gson.toJson(pueblo);
+    }
+
+    // Método PUT para actualizar un pueblo
+    @PutMapping("actualizar/{id}")
+    public String ActualizarCoche(@PathVariable int id, @RequestBody PuebloDto pueblo) {
+        Gson gson = new Gson();
+        for (PuebloDto indice : pueblos) {
+            if(id==indice.getId()){
+                indice=pueblo;
+                break;
+            }
+        }
+    
+        return gson.toJson(pueblo);
+    }
+
+    // Método DELETE para eliminar un pueblo
+    @DeleteMapping("borrar/{id}")
+    public void BorrarCoche(@PathVariable int id){
+        for (PuebloDto indice : pueblos) {
+            if(id==indice.getId()){
+                pueblos.remove(indice);  
+            }
+        }
+
+    }
+    // Método PATCH para actualizar parcialmente
+    @PatchMapping("coches/{id}")
+    public String ActualizarCocheParcial(@PathVariable int id, @RequestBody PuebloDto pueblo){
+        Gson gson = new Gson();
+        for (PuebloDto indice : pueblos) {
+            if(id==indice.getId()){
+                indice=pueblo;
+                break;
+            }
+        }
+    
+        return gson.toJson(pueblo);
+    }
 }
